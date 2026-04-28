43cd06906de685d28375ad753e26854fec43cf50
Drew Peterson <drew2939@outlook.com>
2026-04-27 23:44:48 -0500

    changes before deployment

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/tasksplit">
+    <BrowserRouter basename="/">
       <AppProvider>
         <App />
       </AppProvider>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tasksplit/',   // change to '/' if not using GitHub Pages subfolder
+  base: '/',
 })
