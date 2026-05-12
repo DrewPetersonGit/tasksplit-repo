8f5bf5aa0cf42c83b3ecb3f8223944e4b31f116c
Drew Peterson <drew2939@outlook.com>
2026-05-12 13:39:57 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { AppProvider } from './context/AppContext'
 import './index.css'
+import ReactGA from 'react-ga4'
+
+ReactGA.initialize('G-E2VL31KCRD')
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
